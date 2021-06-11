14a86f6d54a5e4d67237abcd8c993d21f1804080
kapcb <eircccallroot@yeah.net>
2021-06-12 00:38:00 +0800

    framework-base

---
+++ kapcb-framework/src/main/java/com/kapcb/ccc/commons/configuration/RedisConfiguration.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc.commons.configuration;
+
+/**
+ * <a>Title: RedisConfiguration </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: Redis Configuration <a>
+ *
+ * @author Kapcb
+ * @version 1.0.0
+ * @date 2021/6/12 0:37
+ */
+public class RedisConfiguration {
+}
