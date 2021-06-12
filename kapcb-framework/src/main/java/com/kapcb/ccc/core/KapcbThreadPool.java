d9d009954422e3fd50f0ebec0cbe6b0adc0198d2
kapcb <eircccallroot@yeah.net>
2021-06-12 23:43:11 +0800

    SpringCloud-Framework

--- kapcb-framework/src/main/java/com/kapcb/ccc/core/KapcbThreadPool.java
+++ kapcb-framework/src/main/java/com/kapcb/ccc/core/KapcbThreadPool.java
@@ -1,13 +1,13 @@
 package com.kapcb.ccc.core;
 
 /**
- * <a>Title: ThreadPool </a>
+ * <a>Title: KapcbThreadPool </a>
  * <a>Author: Kapcb <a>
- * <a>Description:  <a>
+ * <a>Description: Kapcb Thread Pool <a>
  *
  * @author Kapcb
  * @version 1.0.0
  * @date 2021/6/12 21:09
  */
-public class ThreadPool {
+public class KapcbThreadPool {
 }
