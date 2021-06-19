bf827465efa541e7a0bfce09216cb71241eddb70
kapcb <eircccallroot@yeah.net>
2021-06-19 15:41:38 +0800

    framework

---
+++ kapcb-framework/src/main/java/com/kapcb/ccc/annotation/business/InternalApi.java
@@ -0,0 +1,15 @@
+package com.kapcb.ccc.annotation.business;
+
+/**
+ * <a>Title: InternalApi </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description:  <a>
+ * <p>
+ * 内部服务调用的接口
+ *
+ * @author Kapcb
+ * @version 1.0.0
+ * @date 2021/6/19 15:38
+ */
+public @interface InternalApi {
+}
---
+++ kapcb-framework/src/main/java/com/kapcb/ccc/aspect/ControllerEndPointAspect.java
@@ -0,0 +1,20 @@
+package com.kapcb.ccc.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * <a>Title: ControllerEndPointAspect </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description:  <a>
+ *
+ * @author Kapcb
+ * @version 1.0.0
+ * @date 2021/6/19 15:39
+ */
+@Slf4j
+@Aspect
+public class ControllerEndPointAspect {
+
+
+}
