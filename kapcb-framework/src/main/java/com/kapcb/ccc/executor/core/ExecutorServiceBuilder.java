a71b9ec24fea74024887b808a3b5f57f1a2dca79
kapcb <eircccallroot@yeah.net>
2021-06-20 12:38:19 +0800

    SpringCloud-Frame

---
+++ kapcb-framework/src/main/java/com/kapcb/ccc/executor/core/ExecutorServiceBuilder.java
@@ -0,0 +1,22 @@
+package com.kapcb.ccc.executor.core;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * <a>Title: ExecutorServiceBuilder </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description:  <a>
+ *
+ * @author Kapcb
+ * @version 1.0.0
+ * @date 2021/6/20 12:37
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ExecutorServiceBuilder {
+
+    
+}
