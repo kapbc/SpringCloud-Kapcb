85583d9c3b2ab8f8f3be39329b02971ee9839b6a
kapcb <eircccallroot@yeah.net>
2021-06-22 22:21:27 +0800

    search

--- kapcb-framework/src/main/java/com/kapcb/ccc/properties/RedissonProperties.java
+++ kapcb-framework/src/main/java/com/kapcb/ccc/properties/RedissonProperties.java
@@ -1,12 +1,8 @@
 package com.kapcb.ccc.properties;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 /**
