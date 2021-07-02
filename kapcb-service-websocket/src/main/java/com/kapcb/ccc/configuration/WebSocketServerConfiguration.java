5ec06a91957a6632cb4946449b52425c7a183efa
kapcb <eircccallroot@yeah.net>
2021-07-03 00:21:07 +0800

    websocket

---
+++ kapcb-service-websocket/src/main/java/com/kapcb/ccc/configuration/WebSocketServerConfiguration.java
@@ -0,0 +1,18 @@
+package com.kapcb.ccc.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <a>Title: WebSocketServerConfiguration </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description:  <a>
+ *
+ * @author Kapcb
+ * @version 1.0.0
+ * @date 2021/7/3 0:19
+ */
+@Slf4j
+@Configuration
+public class WebSocketServerConfiguration {
+}
---
+++ kapcb-service-websocket/src/main/java/com/kapcb/ccc/server/WebSocketServer.java
@@ -0,0 +1,19 @@
+package com.kapcb.ccc.server;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ * <a>Title: WebSocketServer </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description:  <a>
+ *
+ * @author Kapcb
+ * @version 1.0.0
+ * @date 2021/7/3 0:20
+ */
+@Slf4j
+@ServerEndpoint("")
+public class WebSocketServer {
+}
