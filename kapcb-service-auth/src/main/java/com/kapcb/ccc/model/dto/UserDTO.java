aaf3b0a6ff0bb084c0f917f74ca38d5ce963bdc2
kapcb <eircccallroot@yeah.net>
2021-07-06 22:22:27 +0800

    auth

---
+++ kapcb-service-auth/src/main/java/com/kapcb/ccc/model/dto/UserDTO.java
@@ -0,0 +1,28 @@
+package com.kapcb.ccc.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * <a>Title: UserDTO </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description:  <a>
+ *
+ * @author Kapcb
+ * @version 1.0.0
+ * @date 2021/7/6 22:19
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class UserDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+
+}
